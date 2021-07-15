c04bb30c5a538960c70ed00b64f0ce6aa98ccd17
Lihongtao2019 <1605510398@qq.com>
2021-07-15 14:58:48 +0800

    v1.new.01

---
+++ 1913089/src/components/inputContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="number" placeholder="0" :id="name" :value="info" />
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    info: String,
+  },
+};
+</script>
+<style scoped>
+</style>
---
+++ 1913089/src/components/speButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="$emit('option')">{{ name }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
